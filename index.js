e99f5edbc2520795275a79ded6633f2843cffe47
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 17:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const token = 'a768bfd7cbd7b8f78dd1e346ad4285f3121c4b5a';
-fetch('https://api.github.com/user/repos', {
+fetch('https://api.github.com/carpe-omnia/repos', {
   headers: {
     Authorization: `token ${token}`
   }
