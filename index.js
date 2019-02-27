a79050cce342b47ab49135569df155aaa4ff665f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 17:20:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+const token = 'not putting this in a public place';
+fetch('https://api.github.com/Carpe-Omnia/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
