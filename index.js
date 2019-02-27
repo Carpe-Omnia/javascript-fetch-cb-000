b8973988572fb6fd7118bcd02b8c8a67c0d49f88
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 17:16:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much.";
+const token = 'a768bfd7cbd7b8f78dd1e346ad4285f3121c4b5a';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+  
